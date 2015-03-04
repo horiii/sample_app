30f1457c69ac119acd8866e52ddf0003df60101d
hori <hori@livesense.co.jp>
2015-03-04 05:01:30 +0000

    add follow-following

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 SampleApp::Application.routes.draw do
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :microposts, only: [:create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   resources :users
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
